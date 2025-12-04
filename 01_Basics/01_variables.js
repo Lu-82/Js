b5ce26a30629209aa375f1935281e13095307a6b
Lu-82 <raghunathmurmu353@gmail.com>
2025-12-04 14:25:46 +0530

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const account_id = 4223
+let account_email ="raghu@example.com";
+var account_password = "12345";
+account_city = "Jamshedpur"
+
+// account_id = 2
+account_email = "murmu@example.com"
+account_password = "4564";
+account_city = "Dhanbad"
+console.log(account_id);
+
+console.table([account_email,account_password,account_city]);
