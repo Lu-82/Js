67a1930c2be64602491c8426373d4f248e100536
Lu-82 <raghunathmurmu353@gmail.com>
2025-12-04 14:36:13 +0530

    data type conversion

---
+++ 01_Basics/03_Data_Type_Con.js
@@ -0,0 +1,34 @@
+
+
+//     1
+
+// let score = 233;
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+
+// let score = "233";
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+
+// let score = "";
+
+// let valueInNumber = Boolean(score)
+// console.log(valueInNumber); // false
+// console.log(typeof valueInNumber); // boolean
+
+// let score = 1;
+
+// let valueInNumber = Boolean(score)
+// console.log(valueInNumber); // true
+// console.log(typeof valueInNumber); // boolean
+
+
+//                  string conversion
+
+let score = 567;
+
+let valueInNumber = String(score)
+console.log(valueInNumber); // 567
+console.log(typeof valueInNumber); // string
