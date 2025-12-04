1720332a3b77749fd3d4107d11bada9ca84d1aa5
Lu-82 <raghunathmurmu353@gmail.com>
2025-12-04 14:26:13 +0530

    data_type

---
+++ 01_Basics/02_data_type.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert(3+3);   we are using node.js not browser
+
+let  a = 123444; // int
+let big = 12345678912233455;// bigint
+let name = "raghu" // string 
+let userLogIn = true // boolean 
+
+let num;
+console.log(num); // num = undefined 
+
+let x = null //NUll (empty)
+let id = Symbol("id");// symbol
+
+let age = 10;
+console.log(typeof x)
